97751cd274f7d8d053e6d0d3ab29658027aa2ff6
Tom Delva <tomtfc8200@gmail.com>
2023-03-27 16:55:47 +0200

    Affichage et stylisation des produits

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,12 +1,21 @@
 import './card.css'
+const logementAll = require('../../Data/data.json');
+const logements = logementAll.map((logement) => 
+    <a href='/' >    
+        <article className='product' key={logement.title}>
+            <img class="image-product" src={logement.cover} alt="logement"/>
+            <h2>{logement.title}</h2>
+        </article>
+</a>    
+)
 
 function Card() {
     return(
-        <div className='card-container'>
-            <div>
-                
+        <section className='product-container'>
+            <div className='product-home'>
+                {logements}
             </div>
-        </div>
+        </section>
     )
 }
 
