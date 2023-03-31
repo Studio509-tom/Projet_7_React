768efd07ffb5d81cc8fdc0a193fd98e55c8f53b1
Tom Delva <tomtfc8200@gmail.com>
2023-03-31 15:10:44 +0200

    End Slider

--- src/components/Slideshow/slideshow.jsx
+++ src/components/Slideshow/slideshow.jsx
@@ -1,8 +1,17 @@
 import styled from "styled-components"
 import './slideshow.css'
+import fontawesome from '@fortawesome/fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft, faChevronRight } from '@fortawesome/fontawesome-free-solid'
+import { useState } from "react"
+
+fontawesome.library.add(faChevronLeft);
+fontawesome.library.add(faChevronRight);
 
 const BackgroundStyled = styled.div`
-    .background-banner{
+    justify-content: space-between;
+
+    .background-slide{
         background : url(${props => props.cover});
         background-position: top -1160px right 0px;
         background-repeat: no-repeat;
@@ -10,17 +19,34 @@ const BackgroundStyled = styled.div`
         background-position: center;
     }
 `
-function Slideshow(props){
-    console.log(props.cover.length)
-    for (let i = 0 ; i < props.cover.length; i++){
-        console.log(i)
+
+function Slideshow(props) {
+
+    const [isCover, setIsCover] = useState(0);
+
+    //Condition si il n'y a qu'une photo
+    if (props.cover.length === 1) {
+        return (
+            <BackgroundStyled cover={props.cover[isCover]} className='parent-slide'>
+                <div className='background-slide'></div>
+                <div className="position">
+                    <p>{isCover + 1}/{props.cover.length}</p>
+                </div>
+            </BackgroundStyled>
+        )
     }
-    return(
-        <BackgroundStyled cover={props.cover} className='parent-slide'>
-            <button className="leftArrow"></button>
-            <div className='background-slide'></div>
-            <button className="rightArrow"></button>
-        </BackgroundStyled>
-    )
+    else {
+        return (
+            <BackgroundStyled cover={props.cover[isCover]} className='parent-slide'>
+                <button onClick={() => props.cover[isCover - 1] === undefined ? setIsCover(props.cover.length - 1) : setIsCover(isCover - 1)} className="buttonArrow arrowLeft"><FontAwesomeIcon className="Arrow" icon="fa-solid fa-chevron-left" /></button>
+                <div className='background-slide'></div>
+                <button onClick={() => props.cover[isCover + 1] === undefined ? setIsCover(0) : setIsCover(isCover + 1)} className="buttonArrow arrowRight"><FontAwesomeIcon className="Arrow" icon="fa-solid fa-chevron-right" /></button>
+                <div className="position">
+                    <p>{isCover + 1}/{props.cover.length}</p>
+                </div>
+            </BackgroundStyled>
+        )
+    }
+
 }
 export default Slideshow
