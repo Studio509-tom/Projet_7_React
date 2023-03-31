a3a8091a06ee314e5d4c283ef4ea32209a216a5b
Tom Delva <tomtfc8200@gmail.com>
2023-03-31 16:57:57 +0200

    Inseretion des élément de la page product

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -3,12 +3,14 @@ const logementAll = require('../../Data/data.json');//Récupération de tout les
 
 //!Affichage pour chaque appartement 
 const logements = logementAll.map((logement) =>
-    <a href={logement.id} key={logement.id}>
+    
         <article className='product' >
-            <img className="image-product" src={logement.cover} alt="logement" />
-            <h2>{logement.title}</h2>
+            <a href={logement.id} key={logement.id}>
+                <img className="image-product" src={logement.cover} alt="logement" />
+                <h2>{logement.title}</h2>
+            </a>
         </article>
-    </a>
+    
 )
 
 //!Affichage de tout les appartement 
--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,22 +1,58 @@
 import Slideshow from "../../components/Slideshow/slideshow";
 import Data from "../../Data/data.json";
+import fontawesome from '@fortawesome/fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/fontawesome-free-solid'
+
+fontawesome.library.add(faStar);
+
 
 function Product() {
+    //Récupération de l'id dans l'url
     const link = (window.location).href;
     var id = link.substring(link.lastIndexOf('/') + 1);
-    // console.log(id)
-    
-    for (let i = 0 ; i < Data.length; i++){
-        if(Data[i].id === id){
+
+    for (let i = 0; i < Data.length; i++) {
+        //Si l'id correspond
+        if (Data[i].id === id) {
             // console.log(Data[i])
-            var slidePicture = Data[i].pictures
-            console.log(slidePicture)
+            var slidePicture = Data[i].pictures;
+            var title = Data[i].title;
+            var location = Data[i].location
+            var tags = Data[i].tags.map((tag) =>
+                <div>
+                    <p>{tag}</p>
+                </div>)
+            var name = Data[i].host.name
+            var pictureProfile = Data[i].host.picture
+            var rating = Data[i].rating
+            var starRating =[]
+            for(let j = 0 ; j< rating; j++){
+                starRating.push(<FontAwesomeIcon icon="fa-solid fa-star" style={{color: "#ff6060",}} />)
+            }
         }
     }
-    
-    return(
+    return (
         <div>
-            <Slideshow cover={slidePicture}/>
+            <Slideshow cover={slidePicture} />
+            <div>
+                <h1>{title}</h1>
+                <p>{location}</p>
+                <div>
+                    {tags}
+                </div>
+            </div>
+            <div>
+                <div>
+                    <p>{name}</p>
+                    <img src={pictureProfile} alt="phto de profile" />
+                </div>
+                <div>
+                    {starRating}
+                </div>
+            </div>
+
+
         </div>
     )
 }
