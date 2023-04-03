1c134f75378c9e527184063e3b0157be546057d3
Tom Delva <tomtfc8200@gmail.com>
2023-04-03 12:38:17 +0200

    Avancement de la page product (finition des collapse)

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -3,6 +3,8 @@ import Data from "../../Data/data.json";
 import fontawesome from '@fortawesome/fontawesome'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/fontawesome-free-solid'
+import Collapse from '../../components/Collapse/collapse'
+import './index.css'
 
 fontawesome.library.add(faStar);
 
@@ -15,43 +17,59 @@ function Product() {
     for (let i = 0; i < Data.length; i++) {
         //Si l'id correspond
         if (Data[i].id === id) {
-            // console.log(Data[i])
-            var slidePicture = Data[i].pictures;
-            var title = Data[i].title;
-            var location = Data[i].location
-            var tags = Data[i].tags.map((tag) =>
+            var slidePicture = Data[i].pictures; // Image du slider
+            var title = Data[i].title;// Titre
+            var location = Data[i].location; // Localisation
+            var tags = Data[i].tags.map((tag) => // Tag de l'annonce
                 <div>
-                    <p>{tag}</p>
+                    <p className="tags">{tag}</p> 
                 </div>)
-            var name = Data[i].host.name
-            var pictureProfile = Data[i].host.picture
-            var rating = Data[i].rating
-            var starRating =[]
-            for(let j = 0 ; j< rating; j++){
-                starRating.push(<FontAwesomeIcon icon="fa-solid fa-star" style={{color: "#ff6060",}} />)
+            var name = Data[i].host.name // Nom de la personne qui poste l'annonce
+            var pictureProfile = Data[i].host.picture // Image de la personne qui poste l'annonce
+            var rating = Data[i].rating // Note de la personne 
+            var starRating = []
+            for (let j = 0; j < rating; j++) { //Affichage du nombre d'étoile coloré en fonction de la note de la personne
+                starRating.push(<FontAwesomeIcon className="star-rating" icon="fa-solid fa-star" style={{ color: "#ff6060", }} />)
             }
+            var ratingNone = 5 - rating ;
+            for (let k = 0 ; k < ratingNone ; k++){ //Affichage des étoiles grisé 
+                starRating.push(<FontAwesomeIcon className="star-rating" icon="fa-solid fa-star" style={{color: "#e3e3e3",}} />)
+            }
+            //Collapse
+            var description = <p className='collapse-description'>{Data[i].description}</p>
+            var stuff =Data[i].equipments.map((element, index) => {
+                return (<p className='collapse-description' key={"equip-"+index.toString()}>{element}</p>)})
+        
         }
     }
+        
+    
     return (
-        <div>
+        <div id="product"> 
             <Slideshow cover={slidePicture} />
-            <div>
-                <h1>{title}</h1>
-                <p>{location}</p>
-                <div>
-                    {tags}
+            <div className="product-id-container">
+                <div className="product-main"> 
+                    <h1 className="product-title">{title}</h1>
+                    <p className="product-location">{location}</p>
+                    <div className="product-tags">
+                        {tags}
+                    </div>
+                </div>
+                <div className="product-profile">
+                    <div className="profile">
+                        <p className="profile-name">{name}</p>
+                        <img className="profile-image" src={pictureProfile} alt="phto de profile" />
+                    </div>
+                    <div className="rating">
+                        {starRating}
+                    </div>
                 </div>
             </div>
-            <div>
-                <div>
-                    <p>{name}</p>
-                    <img src={pictureProfile} alt="phto de profile" />
-                </div>
-                <div>
-                    {starRating}
-                </div>
+            <div className="product-collapse">
+                <Collapse className="collapse-element" params={description} title="Description"/>
+                <Collapse className="collapse-element" params={stuff} title="Equipements"/>
             </div>
-
+            
 
         </div>
     )
