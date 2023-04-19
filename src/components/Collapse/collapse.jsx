8eae8f9d85042cd318bdc7103d51ab36a6d6f098
Tom Delva <tomtfc8200@gmail.com>
2023-04-19 14:57:39 +0200

    END

--- src/components/Collapse/collapse.jsx
+++ src/components/Collapse/collapse.jsx
@@ -14,15 +14,15 @@ const DescStyled = styled.div`
         `
 
 //! Collapse qui utilise un état pour s'ouvrir et se fermé
-function Collapse({params, title}) {
-    const [isOpen , setIsOpen] = useState(false)
+function Collapse({ params, title }) {
+    const [isOpen, setIsOpen] = useState(false)
     // console.log(isOpen)
     return isOpen ? (
         <section className='collapse-container'>
             <article className='collapse' >
-                <div className='collapse-title' onClick={() => setIsOpen (false)}>
+                <div className='collapse-title' onClick={() => setIsOpen(false)}>
                     <h3>{title}</h3>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" /></svg>
                 </div>
                 <DescStyled >
                     {params}
@@ -33,9 +33,9 @@ function Collapse({params, title}) {
     ) : (
         <section className='collapse-container'>
             <article className='collapse' >
-                <div className='collapse-title' onClick={() => setIsOpen (true)}>
+                <div className='collapse-title' onClick={() => setIsOpen(true)}>
                     <h3>{title}</h3>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" /></svg>
                 </div>
             </article>
 
