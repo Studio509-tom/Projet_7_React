7648aafeb5a5ecf7afbab37f92b8bf9fc0569f0b
Tom Delva <tomtfc8200@gmail.com>
2023-04-19 10:23:24 +0200

    End V1.3

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import styled from 'styled-components'
 import Banner from '../../components/Banner/banner'
 import Card from '../../components/Card/card'
